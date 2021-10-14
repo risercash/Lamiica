37e4ae33d733213cdd3297ba09e4bf96282a6f36
risercash <risercash@gmail.com>
2021-10-14 15:12:56 +0600

    V1

---
+++ config.py
@@ -0,0 +1,12 @@
+user_admin_id = [490073120, 1434266116]
+
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+URL = os.getenv("URL")
+ADM = os.getenv("ADM")
+ME = os.getenv("ME")
